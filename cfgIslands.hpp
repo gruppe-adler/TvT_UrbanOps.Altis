0dde570444ad58989e0f39e2de951b8440c8a31d
McDiod <hobbeldobbel@gmail.com>
2016-12-19 16:50:09 +0100

    island configs

--- cfgIslands.hpp
+++ cfgIslands.hpp
@@ -1,8 +1,99 @@
 class CfgIslands {
 	class Altis {
-		displayName = Altis;
+		displayName = "Altis";
 		isWoodland = true;
-		spawnPosBlu[] = {13754.7,6389.77,0};
-		spawnPosOpf[] = {13836.3,6765.53,0};
+		spawnPosBlu[] = {8454.05,25140.7,0};
+		spawnPosOpf[] = {8712.9,25205.4,0};
+	};
+
+	class Beketov {
+		displayName = "Beketov";
+		isWoodland = true;
+		spawnPosBlu[] = {-258.376,308.728,0};
+		spawnPosOpf[] = {-249.061,955.673,0};
+	};
+
+	class Chernarus {
+		displayName = "Chernarus";
+		isWoodland = true;
+		spawnPosBlu[] = {15074.6,14748.2,0};
+		spawnPosOpf[] = {15128.1,13945.9,0};
+	};
+
+	class Chernarus_winter {
+		displayName = "Chernarus Winter";
+		isWoodland = true;
+		spawnPosBlu[] = {15074.6,14748.2,0};
+		spawnPosOpf[] = {15128.1,13945.9,0};
+	};
+
+	class fata {
+		displayName = "Fata";
+		isWoodland = false;
+		spawnPosBlu[] = {10616.3,8942.11,0};
+		spawnPosOpf[] = {10723.9,10105,0};
+	};
+
+	class mbg_celle2 {
+		displayName = "Celle";
+		isWoodland = true;
+		spawnPosBlu[] = {13121.7,12175.6,0};
+		spawnPosOpf[] = {12488.2,12689.2,0};
+	};
+
+	class Stratis {
+		displayName = "Stratis";
+		isWoodland = true;
+		spawnPosBlu[] = {7093.92,5945.18,0};
+		spawnPosOpf[] = {7102.51,5981.09,0};
+	};
+
+	class Sara_dbe1 {
+		displayName = "United Sahrani";
+		isWoodland = true;
+		spawnPosBlu[] = {17997.3,18808.7,0};
+		spawnPosOpf[] = {18177.5,18120.4,0};
+	};
+
+	class Shapur_BAF {
+		displayName = "Shapur";
+		isWoodland = false;
+		spawnPosBlu[] = {2881.15,1992.91,0};
+		spawnPosOpf[] = {2626.44,2236.07,0};
+	};
+
+	class Takistan {
+		displayName = "Takistan";
+		isWoodland = false;
+		spawnPosBlu[] = {13662.5,3886.99,0};
+		spawnPosOpf[] = {13727.4,4748.37,0};
+	};
+
+	class Tanoa {
+		displayName = "Tanoa";
+		isWoodland = true;
+		spawnPosBlu[] = {1389.42,947.902,0};
+		spawnPosOpf[] = {1150.11,593.379,0};
+	};
+
+	class utes {
+		displayName = "Utes";
+		isWoodland = true;
+		spawnPosBlu[] = {4377.08,2324.57,0};
+		spawnPosOpf[] = {4315.79,2452.54,0};
+	};
+
+	class vt5 {
+		displayName = "vt5";
+		isWoodland = true;
+		spawnPosBlu[] = {5709.2,6239.52,0};
+		spawnPosOpf[] = {6156.56,5841.48,0};
+	};
+
+	class Zargabad {
+		displayName = "Zargabad";
+		isWoodland = false;
+		spawnPosBlu[] = {7396.98,5339.07,0};
+		spawnPosOpf[] = {7756.23,5303.44,0};
 	};
 };
