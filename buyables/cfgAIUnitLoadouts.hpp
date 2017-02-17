e7d8fc4af089c9b967bc37f77debcd499110474f
McDiod <hobbeldobbel@gmail.com>
2017-02-17 17:28:37 +0100

    enable aicommand

--- buyables/cfgAIUnitLoadouts.hpp
+++ buyables/cfgAIUnitLoadouts.hpp
@@ -1,7 +1,17 @@
 class cfgAIUnitLoadouts {
     class ind_me_terrorists {
         class marksman {
-            uniform = "rhs_uniform_df15";
+            uniform[] = {
+                "LOP_U_AM_Fatigue_01_3",
+                "LOP_U_AM_Fatigue_02_5",
+                "LOP_U_AM_Fatigue_01_5",
+                "LOP_U_AM_Fatigue_02_6",
+                "LOP_U_AM_Fatigue_02_2",
+                "LOP_U_AM_Fatigue_02",
+                "LOP_U_AM_Fatigue_01"
+            };
+            vest = "";
+            backpack = "";
         };
     };
 };
