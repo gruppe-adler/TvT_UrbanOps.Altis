37c9501c21d0b14163943ea663d528e7586af7d4
McDiod <hobbeldobbel@gmail.com>
2017-02-19 21:35:00 +0100

    add lock pick to terrorists buyables

--- buyables/blu_ind_me_terrorists/items.hpp
+++ buyables/blu_ind_me_terrorists/items.hpp
@@ -39,6 +39,13 @@ class Items {
         stock = 20;
     };
 
+    class ACE_key_lockpick {
+        displayName = "Lock Pick Set";
+        description = "Can be used to pick the locks of civilian or enemy vehicles. One lock pick set can be used to unlock multiple vehicles."
+        price = 500;
+        stock = 10;
+    };
+
     class ACE_DefusalKit {
         displayName = "Defusal Kit";
         price = 100;
