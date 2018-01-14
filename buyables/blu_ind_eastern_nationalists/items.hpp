947533afd4bb01c44455ca1d40376ba250e6dd04
McDiod <hobbeldobbel@gmail.com>
2018-01-14 16:54:12 +0100

    remove medical items from buyables

--- buyables/blu_ind_eastern_nationalists/items.hpp
+++ buyables/blu_ind_eastern_nationalists/items.hpp
@@ -2,37 +2,6 @@ class Items {
     displayName = "Items";
     kindOf = "Items";
 
-    class ACE_fieldDressing {
-        displayName = "Bandages (x10)";
-        description = "10 simple bandages in sterile packaging.";
-        amount = 10;
-        price = 100;
-        stock = 40;
-    };
-
-    class ACE_morphine {
-        displayName = "Morphine (x5)";
-        description = "5 morphine autoinjectors. Heavy painkiller.";
-        amount = 5;
-        price = 100;
-        stock = 40;
-    };
-
-    class ACE_epinephrine {
-        displayName = "Epinephrine (x5)";
-        description = "5 epipen autoinjectors. Raises heart rate of patient.";
-        amount = 5;
-        price = 100;
-        stock = 40;
-    };
-
-    class ACE_bloodIV_250 {
-        displayName = "Blood Bag (250ml)";
-        description = "250ml AB+ type blood in a blood bag with IV.";
-        price = 100;
-        stock = 40;
-    };
-
     class ACE_RangeCard {
         displayName = "Range Card";
         price = 100;
